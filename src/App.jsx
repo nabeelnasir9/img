2c8da66b88320db0b5676c7323d9757909e40f73
Nabeel Nasir <69030093+nabeelnasir9@users.noreply.github.com>
2023-12-07 00:23:50 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Image from '../src/assets/psimg.jpeg';
+function App() {
+
+  return (
+    <>
+    <img src={Image} alt="img" style={{width:'100%', height:'100%'}}/>
+    </>
+  )
+}
+
+export default App
